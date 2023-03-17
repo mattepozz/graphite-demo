ad3cfdbed285e00977042cee405dbd5bd5603e51
Matteo <mpozzetti@evernote.com>
2023-03-17 15:31:59 +0000

    feat: manifesto

--- graphite_demo/About_us.py
+++ graphite_demo/About_us.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.write("# Bending Spoons")
 st.write("We make apps")
 st.write("Ah, we also make spaghetti")
+st.write("Check out our values in the Manifesto page")
---
+++ graphite_demo/pages/Manifesto.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write("# Our values")
+st.write("## We are climbers")
+st.image("graphite_demo/resources/highlander.jpeg")
