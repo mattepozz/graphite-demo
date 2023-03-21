874f89b502fa9d9030cdf40d6d7531d56d99a151
Matteo <mpozzetti@evernote.com>
2023-03-21 17:53:36 +0000

    feat: manifesto

--- graphite_demo/About_us.py
+++ graphite_demo/About_us.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.write("# Bending Spoons")
 st.write("We make apps!")
 st.write("We also cook vegan food and trade crypto currencies.")
+st.write("Check out our values in the Manifesto page.")
